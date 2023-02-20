368c15314c60e425353f621a9fd1200087bc3feb
AndresCueva <carlos9125@gmail.com>
2023-02-20 18:23:29 -0500

    Update provider.tf

--- prod/provider.tf
+++ prod/provider.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~>4.16"
     }
   }
+  backend "s3" {
+    bucket         = "microsvc-terraform-state"
+    key            = "prod"
+    region         = "eu-west-1"
+    dynamodb_table = "microsvc-terraform-state"
+  }
 }
