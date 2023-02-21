702724f50b5e978d66999a8259e5d4e8b18cc63c
andrescueva <carlos9125@gmail.com>
2023-02-20 19:05:29 -0500

    set output lb dns name

--- modules/application_lb/outputs.tf
+++ modules/application_lb/outputs.tf
@@ -8,5 +8,8 @@ output "arn" {
 
 output "target_group_arn" {
   value = aws_lb_target_group.target.arn
+}
 
+output "dns_name" {
+  value = aws_lb.lb.dns_name
 }
--- prod/outputs.tf
+++ prod/outputs.tf
@@ -5,3 +5,7 @@ output "ecs_task_role_arn" {
 output "ecr_repository_url" {
   value = module.ecr.url
 }
+
+output "lb_dns_name" {
+  value = module.lb.dns_name
+}
